106276c1c78b3b1796003c658f5bbacba2b15be8
KKHADIJAA008 <kkhadijaa008@gmail.com>
2023-06-15 18:44:33 -0700

    puts-update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - C puts function. 
+ * main - puts function
  * Return: 0 (Success)
  */
 int main(void)
@@ -8,3 +8,4 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+	
