af919d5cbb47e1d1fd81e27ebb442315b378c017
KKHADIJAA008 <kkhadijaa008@gmail.com>
2023-06-15 20:01:15 -0700

    4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int H;
 long int A;
 long long int D;
 float J;
-printf("Size of char: %lu 1 byte(s)\n", (unsigned long)sizeof(K));
-printf("Size of an int: %lu 4 byte(s)\n", (unsigned long)sizeof(H));
-printf("Size of a long int: %lu 4 byte(s)\n", (unsigned long)sizeof(A));
-printf("Size of a long long int: %lu 8 byte(s)\n", (unsigned long)sizeof(D));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(K));
+printf("Size of an int: %lu  byte(s)\n", (unsigned long)sizeof(H));
+printf("Size of a long int: %lu  byte(s)\n", (unsigned long)sizeof(A));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(D));
 printf("Size of a float: %lu 4 byte(s)\n", (unsigned long)sizeof(J));
 return (0);
 }
