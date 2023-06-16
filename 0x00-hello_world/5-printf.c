3295e05f6754f6a6be136cbce93db876f3fff7a4
KKHADIJAA008 <kkhadijaa008@gmail.com>
2023-06-15 19:10:24 -0700

    printf-c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - printf function test
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
